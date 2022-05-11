636690b20ed7e5696e789cb5de2c0462ddf8ff8f
Naveen-Prathap <naveenprathape@gmail.com>
2022-05-11 12:13:17 +0530

    Running on server

--- scheduler.py
+++ scheduler.py
@@ -22,7 +22,8 @@ def main(r: redis.Redis)-> None:
                 worker  = json.loads(r.rpop('free_workers'))
                 job = json.loads(r.rpop('jobs_to_schedule'))
                 job['worker_id'] = worker['worker_id']
-                r.lpush(f'urls{job["template_name"]}', json.dumps({job['base_url'] : 'parse_listing_page'}))
+                if not r.sismember('visited'+job['template_name'], job['base_url']):
+                    r.lpush(f'urls{job["template_name"]}', json.dumps({job['base_url'] : 'parse_listing_page'}))
                 r.lpush(worker['server_id'], json.dumps(job))
                 r.lpush('scheduled_jobs', json.dumps({job['template_name']: worker['worker_id'] }))
             else:
@@ -35,3 +36,4 @@ def main(r: redis.Redis)-> None:
 
 # r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 r = redis.Redis(host='172.16.220.64', port=6379, db=0, decode_responses=True)
+main(r)

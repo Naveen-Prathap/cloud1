28b07612546dec93034c95d69bf682e9572b09e9
Naveen-Prathap <naveenprathape@gmail.com>
2022-05-11 12:20:01 +0530

    Running on server 2

--- redis_machan.py
+++ redis_machan.py
@@ -2,5 +2,8 @@ import redis
 
 r = redis.Redis(host='172.16.220.64', port=6379, db=0, decode_responses=True)
 while True:
-    for key in r.scan_iter("user:*"):
-        print(r.llen(key))
+    for key in r.keys():
+        try:
+            print(f'{key} : {r.llen(key)}')
+        except:
+            pass
